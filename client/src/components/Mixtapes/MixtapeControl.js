c5004f2bb5e9aa1e1886c061500d01a989de14af
Drake Wilcox <drakewilcox@gmail.com>
2020-05-18 16:50:26 -0700

    create boilerplate for MixTapeControl which will hold firebase.

---
+++ client/src/components/Mixtapes/MixtapeControl.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase';
+import { useSelector } from 'react-redux';
+import firebase from 'firebase/app';
+import { v4 } from 'uuid';
+import React, {Component} from 'react'; 
+import { Link } from 'react-router-dom'; 
+
+function MixtapeControl(props) {
+
+
+return(
+  props
+)
+  
+}
+
+export default MixtapeControl;
