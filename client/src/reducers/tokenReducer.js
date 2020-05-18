2aea7a24eeb3f263429367b57ca801d7e2743bb3
Drake Wilcox <drakewilcox@gmail.com>
2020-05-18 10:10:05 -0700

    pass second test for setToken reducer.

--- client/src/reducers/tokenReducer.js
+++ client/src/reducers/tokenReducer.js
@@ -1,8 +1,18 @@
+import * as c from '../actions/ActionTypes.js';
+
 const initialState = {
   username: null, 
   accessToken: null
 }
 
 export default (state=initialState, action) => {
+  switch (action.type) {
+    case c.SET_TOKEN: 
+      return Object.assign({}, state, {
+        accessToken: action.token
+      })
+  }
+
+
   return state
 }
