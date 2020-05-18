dbe0e15c3efd586010efa7e91327e34078aa1c0f
Drake Wilcox <drakewilcox@gmail.com>
2020-05-18 09:42:34 -0700

    add reducers and tests, and confirm first meaningful fail.

---
+++ client/src/__tests__/reducers/token-reducer.test.js
@@ -0,0 +1,21 @@
+import tokenReducer from '../../reducers/tokenReducer';
+// import * as c from './../../actions/ActionTypes';
+
+describe('parksReducer', () => {
+  
+  const defaultState = {
+    userName: null, 
+    accessToken: null
+  };
+
+  test('should successfully return default state if no action is passed into it', () => {
+    expect(tokenReducer(defaultState, {type: null})).toEqual(
+      {
+        username: null, 
+        accessToken: null, 
+      }
+    );
+  });
+
+  
+})
---
+++ client/src/reducers/tokenReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+  userName: null, 
+  accessToken: null
+}
+
+export default (state=initialState, action) => {
+  return state
+}
