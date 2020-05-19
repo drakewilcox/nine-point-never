dfa4c4dfced47314112f2a781276e0da7e56c7b3
Drake Wilcox <drakewilcox@gmail.com>
2020-05-18 19:42:47 -0700

    implement firestore to state and maped response to Mixtape component.

--- client/src/components/Mixtapes/MixtapeList.js
+++ client/src/components/Mixtapes/MixtapeList.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import Mixtape from './Mixtape';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
-class MixtapeList extends Component {
-  render() {
-    return (
-      <div>
+function MixtapeList(props) {
 
-      </div>
-    );
+  useFirestoreConnect([ { collection: 'mixtapes' }]); 
+  const mixtapes = useSelector((state) => state.firestore.ordered.mixtapes);
+
+  let renderList; 
+
+  if(isLoaded(mixtapes)) {
+    renderList = mixtapes.map((a) => <Mixtape surveyObj={a} key={a.id} /> );
+  } else {
+    renderList = 'loading...';
   }
+
+  return ( 
+    <React.Fragment>
+      <h1>Playlists:</h1>
+      <div>{renderList}</div>
+    </React.Fragment>
+  )
 }
 
 export default MixtapeList 
