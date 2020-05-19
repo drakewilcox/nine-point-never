2d69bf0467e19f6891933f5ddaa7921d0a73c81a
Drake Wilcox <drakewilcox@gmail.com>
2020-05-19 09:38:11 -0700

    create action for fetching playlist cover photo.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,3 +10,16 @@ export const setUsername = (username) => ({
   username
 });
 
+export const makeApiCaall = (id, token) => {
+  return dispatch => {
+    return fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/images`)
+    .then(response => response.json())
+    .then((jsonifiedResponse) => {
+      dispatch(getImageSuccess(jsonifiedResponse));
+    })
+    .catch((error) => {
+      dispatch(getImageFailure(error));
+    });
+  }
+}
+
