82275f58fa6ae494bc5567ce4aea9b3414cd575c
Drake Wilcox <drakewilcox@gmail.com>
2020-05-19 09:53:17 -0700

    refactor makeApiCall action to include headers.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,7 +12,11 @@ export const setUsername = (username) => ({
 
 export const makeApiCaall = (id, token) => {
   return dispatch => {
-    return fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/images`)
+    const request = new Request(`https://api.spotify.com/v1/playlists/${id}/images`, {
+      headers: new Headers({
+        'Authorization': 'Bearer ' + token
+      })
+    })
     .then(response => response.json())
     .then((jsonifiedResponse) => {
       dispatch(getImageSuccess(jsonifiedResponse));
