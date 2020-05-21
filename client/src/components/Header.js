eff3d699168150b01bc55974c8f31fbd8a3a983c
Drake Wilcox <drakewilcox@gmail.com>
2020-05-21 14:30:23 -0700

    add button and change height to header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-
+import { Button } from 'react-bootstrap';
 const heroImage = {
   backgroundImage: 'linear-gradient(rgba(0,0,0,0.5), rgba(0, 0, 0, 0.5)), url(https://images.unsplash.com/photo-1571330735066-03aaa9429d89?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80)', 
   height: '100%',
@@ -12,11 +12,8 @@ const heroImage = {
 
 const heroText = {
   textAlign: 'center', 
-  padding: '100px',
+  padding: '30px',
   position: 'relative', 
-  // top: '50%', 
-  // left: '50%', 
-  // transform: 'translate(-50%, -50%)', 
   color: 'white'
 }
 
@@ -26,7 +23,7 @@ function Header() {
       <div style={heroText}>
         <h1>90 Point Never</h1>
         <p>Curated Playlists For Music Discovery</p>
-        {/* <button> Sign In With Spotify </button> */}
+        <Button variant='dark'href='http://localhost:8888'> Sign In With Spotify </Button>
       </div>
     </div>
   )
